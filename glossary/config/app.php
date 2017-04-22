23d7c721975651df97a15304a5bf341d4df3c39c
Balázs Meskó <meskobalazs@gmail.com>
2017-04-22 13:34:30 +0200

    MySQL-specifikus beállítás

--- glossary/config/app.php
+++ glossary/config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'glossary'),
 
     /*
     |--------------------------------------------------------------------------
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'CET',
 
     /*
     |--------------------------------------------------------------------------
@@ -77,7 +77,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'hu',
 
     /*
     |--------------------------------------------------------------------------
--- glossary/routes/web.php
+++ glossary/routes/web.php
@@ -14,7 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/hello', function () {
-    return view('hello');
-});
